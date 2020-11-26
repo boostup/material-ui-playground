1440366a480039866f81f27ec76a2f6bacd9e5ea
boostup <boostup@gmail.com>
2020-11-26 18:00:58 +0100

    integrated a backdrop with loading animation

--- src/layout/MainLayoutPageWrapper/index.jsx
+++ src/layout/MainLayoutPageWrapper/index.jsx
@@ -7,6 +7,7 @@ import Body from "../Body";
 
 import { CssBaseline } from "@material-ui/core";
 import { useStyles } from "../layout.styles";
+import Backdrop from "layout/Backdrop/index";
 
 function MainLayoutPageWrapper({ title, isLoading, children }) {
   const classes = useStyles();
@@ -23,7 +24,12 @@ function MainLayoutPageWrapper({ title, isLoading, children }) {
       <HtmlHeadTitle title={title} />
       <Header onMenuIconClicked={handleDrawerToggle} />
       <Sidebar isOpen={mobileOpen} onClose={handleDrawerToggle} />
-      <Body>{children}</Body>
+      <Body>
+        <p>
+          <Backdrop />
+        </p>
+        {children}
+      </Body>
     </div>
   );
 }
